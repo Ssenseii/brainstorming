3cda556e7b316bd99a5f7f65a75b1d0df14144e5
Simon Reinhardt <saadabou.study@gmail.com>
2022-08-23 17:05:19 +0100

    hopefully this works

--- app/js/script.js
+++ app/js/script.js
@@ -1,23 +1,14 @@
 /// calc
 
 /// getting window height and width
-var body = document.body,
-  html = document.documentElement;
-let winHeight = Math.max(body.scrollHeight, body.offsetHeight,
-  html.clientHeight, html.scrollHeight, html.offsetHeight);
+var body = document.body, html = document.documentElement;
+
+let winHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
 let winWidth = window.innerWidth;
 
 /// calculating grid height and width
 let gridHeight = winHeight / 10;
 let gridWidth = winWidth / 20;
-
-/// changing grid__element width and height to corresponding ones above
-
-let gridElement = document.getElementById("element");
-
-
-gridElement.style.setProperty('--grid-width', gridWidth + "px")
-gridElement.style.setProperty('--grid-height', gridHeight + "px")
 
 /// adding and appending
 let row = `<div class="grid__element"></div>
@@ -43,11 +34,11 @@ let row = `<div class="grid__element"></div>
 
     let element = document.getElementById("element");
 
-    let i = winHeight / gridWidth;
+    let i = winHeight / gridHeight;
 
 /// changing number of grid rows
     let grid = document.getElementById("grid");
-    let rowNumber = Math.trunc(i)
+    let rowNumber = Math.floor(i)
     grid.style.setProperty('--row-number', rowNumber + 1)
 
 
@@ -56,7 +47,13 @@ let row = `<div class="grid__element"></div>
       console.log("j:" + j)
     }
 
+/// changing grid__element width and height to corresponding ones above
 
+let gridElement = document.getElementsByClassName("grid__element");
+for (var index = 0 ; index < gridElement.length ; index++){  
+  gridElement[index].style.setProperty('--grid-width', gridWidth + "px")
+  gridElement[index].style.setProperty('--grid-height', gridHeight + "px")
+}
 
     /// height update issue resolved using this code
 
@@ -68,9 +65,9 @@ window.addEventListener('resize', function () {
 
 /// debugging
 
-console.log( "windHeight " + winHeight)
-console.log( "winWidth " + winWidth)
-console.log( "gridHeight " + gridHeight)
+console.log( "windHeight =" + winHeight)
+console.log( "winWidth =" + winWidth)
+console.log( "gridHeight ==" + gridHeight)
 console.log( "gridWidth " + gridWidth)
-console.log("i " + i)
-console.log("row Number " + rowNumber)
+console.log("i =" + i)
+console.log("row Number =" + rowNumber)
